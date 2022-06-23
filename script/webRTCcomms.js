7ab31b9355864d97e4ed8e689c760ff136c29c41
tkoterwas <ted@superational.com>
2022-06-23 14:40:39 +0100

    added comments to clarify how it all works and reorganised the code a bit

--- script/webRTCcomms.js
+++ script/webRTCcomms.js
@@ -1,7 +1,7 @@
 var lastPeerId = null;
 var firstPeerCreated = false;
 var peer = null; // own peer object
-var recvId = idFromHash();//"c5dwqeqqb2808-e7ea-4a3f-82eb-a8a8bb905eea";
+var firstPeerId = idFromHash();//"c5dwqeqqb2808-e7ea-4a3f-82eb-a8a8bb905eea";
 var locationName = locationNameFromHash();
 var remoteLocationName = getRemoteLocationName();
 var conn = null;
@@ -11,6 +11,10 @@ var clearMsgsButton = document.getElementById("clearMsgsButton");
 var testMidiButton = document.getElementById("testMidi");
 var recordButton = document.getElementById("record");
 var playButton = document.getElementById("play");
+
+/**
+ * Get IDs and names from the url
+ */
 
 console.log(window.location.hash);
 
@@ -29,13 +33,15 @@ function getRemoteLocationName() {
     return "There";
 }
 
+/**
+ * MIDI
+ */
+
 import { MIDI, MIDIEvent } from "./MIDI.js";
 import { MIDISequencer } from "./MIDISequencer.js";
 var midiDevice = null;
 var midi = new MIDI(handleMidiEventFromLocal); 
-/**
- * Initialise MIDI and define handlers for locol MIDI events
- */
+
 midi.initialize().then(() => {
     console.log("initialised")
     console.log(midi.inputs)
@@ -64,11 +70,7 @@ function handleMidiEventFromRemote(data) {
 }
 
 function sendMidiEventToRemote(data) {
-    if (conn && conn.open) {
-        conn.send(data);
-    } else {
-        console.log('Connection is closed');
-    }
+    broadcastToPeers(data)
     const msg = describeData(data);
     if (msg) addMessage(locationName + ": " + msg, "selfMsg");
 }
@@ -88,134 +90,9 @@ function testMidi() {
     setTimeout(sendMidiEventToRemote, 500, noteOff); 
 }
 
-testMidiButton.addEventListener('click', testMidi);
-
-var midiSequencer = new MIDISequencer(sendMidiEventToLocal);
-
-recordButton.addEventListener('click', function () {
-    midiSequencer.record(); 
-});
-
-playButton.addEventListener('click', function () {
-    midiSequencer.stop();
-    midiSequencer.play(); 
-});
-
-document.addEventListener('keypress', function(e) {
-    var event = e || window.event;
-    var char = event.which || event.keyCode;
-    if (char == 109)
-        testMidi();
-});
-
 /**
- * Create the Peer object for our end of the connection.
- *
- * Sets up callbacks that handle any events related to our
- * peer object.
+ * add messages to the screen
  */
-function initialize() {
-    status.innerHTML = "Awaiting connection.."
-    // Create own peer object with connection to shared PeerJS server
-    let peerId = (firstPeerCreated)? null: recvId;
-    peer = new Peer(peerId, {
-        debug: 2
-    });
-
-    peer.on('open', function (id) {
-        // Workaround for peer.reconnect deleting previous id
-        
-        if (peer.id === null) {
-            console.log('Received null id from peer open');
-            peer.id = lastPeerId;
-        } else {
-            lastPeerId = peer.id;
-        }
-        if (firstPeerCreated) join();
-    });
-    peer.on('connection', function (c) {
-        // Allow only a single connection
-        // if (conn && conn.open) {
-        //     c.on('open', function() {
-        //         c.send("Already connected to another client");
-        //         setTimeout(function() { c.close(); }, 500);
-        //     });
-        //     return;
-        // }
-
-        conn = c;
-        status.innerHTML = ""; //"Connected";
-        if (!firstPeerCreated) ready();
-    });
-    peer.on('disconnected', function () {
-        status.innerHTML = "Connection lost. Please reconnect";
-        console.log('Connection lost. Please reconnect');
-        peer.reconnect();
-    });
-    peer.on('close', function() {
-        conn = null;
-        status.innerHTML = "Connection destroyed. Please refresh";
-        console.log('Connection destroyed');
-        if (firstPeerCreated) initialize();
-    });
-    peer.on('error', function (err) {
-        console.log(err);
-        if (err.message.includes("is taken")) {
-            firstPeerCreated = true;
-        }
-    });
-};
-
-/**
- * Create the connection between the two Peers.
- *
- * Sets up callbacks that handle any events related to the
- * connection and data received on it.
- */
-function join() {
-    // Close old connection
-    if (conn) {
-        conn.close();
-    }
-
-    // Create connection to destination peer specified in the input field
-    conn = peer.connect(recvId, {
-        reliable: false
-    });
-
-    conn.on('open', function () {
-        status.innerHTML = "" //"Connected";
-    });
-    // Handle incoming data (messages only since this is the signal sender)
-    conn.on('data', function (data) {
-        if (typeof(data)=== "string") {
-            addMessage(remoteLocationName + ": " + msg, "peerMsg");
-        } 
-        else if (typeof(data)=== "object") {
-            handleMidiEventFromRemote(data);
-        } 
-    });
-    conn.on('close', function () {
-        status.innerHTML = "Connection closed";
-    });
-    connectVideo();
-};
-
-function ready() {
-    conn.on('data', function (data) {
-        if (typeof(data)=== "string") {
-            addMessage(remoteLocationName + ": " + msg, "peerMsg");
-        } 
-        else if (typeof(data)=== "object") {
-            handleMidiEventFromRemote(data);
-        } 
-    });
-    conn.on('close', function () {
-        status.innerHTML = "Connection reset<br>Awaiting connection...";
-        conn = null;
-    });
-    connectVideo();
-}
 
 function addMessage(msg, className) {
     var now = new Date();
@@ -245,6 +122,156 @@ function clearMessages() {
 // Clear messages box
 clearMsgsButton.addEventListener('click', clearMessages);
 
+testMidiButton.addEventListener('click', testMidi);
+
+var midiSequencer = new MIDISequencer(sendMidiEventToLocal);
+
+recordButton.addEventListener('click', function () {
+    midiSequencer.record(); 
+});
+
+playButton.addEventListener('click', function () {
+    midiSequencer.stop();
+    midiSequencer.play(); 
+});
+
+document.addEventListener('keypress', function(e) {
+    var event = e || window.event;
+    var char = event.which || event.keyCode;
+    if (char == 109)
+        testMidi();
+});
+
+/**
+ * WebRTC for sending MIDI. 
+ */
+
+/**
+ * Initialise this side's peer object
+ */
+function initialize() {
+    status.innerHTML = "Awaiting connection.."
+    // Try to create (the constructor tries to connect to the PeerServer) 
+    // this side's peer object using the firstPeerId (this is necessary because
+    // the other browsers running this script will need to know what peerID to connect to but we dont know if 
+    // one of them has already been created until we get an error that the id is already taken.
+    // that error makes the firstPeerCreated flag true, in which case we create a peer without 
+    // specifying an ID as one will be created for it by the PeerServer 
+    let peerId = (firstPeerCreated)? null: firstPeerId;
+    peer = new Peer(peerId, {
+        debug: 2
+    });
+    /** 
+     * Emitted when a connection to the PeerServer is established.
+    */
+    peer.on('open', function (id) {
+        // Workaround for peer.reconnect deleting previous id   
+        if (peer.id === null) {
+            console.log('Received null id from peer open');
+            peer.id = lastPeerId;
+        } else {
+            lastPeerId = peer.id;
+        }
+        // if the firstPeer had already been created when we created this one, 
+        // call our join function to establish a connection to it
+        if (firstPeerCreated) join();
+    });
+    /** 
+     *  called when a remote peer tries to connect to our ID
+     * */
+    peer.on('connection', function (c) {
+        // Allow only a single connection
+        // if (conn && conn.open) {
+        //     c.on('open', function() {
+        //         c.send("Already connected to another client");
+        //         setTimeout(function() { c.close(); }, 500);
+        //     });
+        //     return;
+        // }
+
+        conn = c;
+        status.innerHTML = ""; //"Connected";
+        // if the firstPeer had NOT already been created when we created this one
+        // then this is the first peer so call our ready function to finish setting
+        // it up without trying to connect to another peer
+        if (!firstPeerCreated) ready();
+    });
+    peer.on('disconnected', function () {
+        status.innerHTML = "Connection lost. Please reconnect";
+        console.log('Connection lost. Please reconnect');
+        peer.reconnect();
+    });
+    peer.on('close', function() {
+        conn = null;
+        status.innerHTML = "Connection destroyed. Please refresh";
+        console.log('Connection destroyed');
+        if (firstPeerCreated) initialize();
+    });
+    peer.on('error', function (err) {
+        console.log(err);
+        if (err.message.includes("is taken")) {
+            firstPeerCreated = true;
+        }
+    });
+};
+
+/**
+ * Create a connection to the firstPeer.
+ */
+function join() {
+    // Close old connection
+    if (conn) {
+        conn.close();
+    }
+    // create the connection. set the reliable flag so that it drops data 
+    // rather than queing it (which will cause a backlog of MIDI data that all comes at once)
+    conn = peer.connect(firstPeerId, {
+        reliable: false
+    });
+
+    conn.on('open', function () {
+        status.innerHTML = "" //"Connected";
+    });
+    // Handle incoming data 
+    conn.on('data', function (data) {
+        if (typeof(data)=== "string") {
+            addMessage(remoteLocationName + ": " + msg, "peerMsg");
+        } 
+        else if (typeof(data)=== "object") {
+            handleMidiEventFromRemote(data);
+        } 
+    });
+    conn.on('close', function () {
+        status.innerHTML = "Connection closed";
+    });
+    connectVideo();
+};
+/**
+ * Finish setting up the first peer after it was successfully connected to the PeerServer.
+ */
+function ready() { 
+    conn.on('data', function (data) {
+        if (typeof(data)=== "string") {
+            addMessage(remoteLocationName + ": " + msg, "peerMsg");
+        } 
+        else if (typeof(data)=== "object") {
+            handleMidiEventFromRemote(data);
+        } 
+    });
+    conn.on('close', function () {
+        status.innerHTML = "Connection reset<br>Awaiting connection...";
+        conn = null;
+    });
+    connectVideo();
+}
+
+function broadcastToPeers(data) {
+    if (conn && conn.open) {
+        conn.send(data);
+    } else {
+        console.log('Connection is closed');
+    }
+}
 
 /**
  * Video call
@@ -280,7 +307,7 @@ async function tryToConnectVideo() {
     try {
         mediaStream = await navigator.mediaDevices.getUserMedia(constraints);
         if (firstPeerCreated) {
-            call = peer.call(recvId, mediaStream);
+            call = peer.call(firstPeerId, mediaStream);
             displayCall();
         }
         else {
